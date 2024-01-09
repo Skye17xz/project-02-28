cb208e9fbc713a6f960902cdfd38c4daea9815ae
Kongphop <kongphopxz@gmail.com>
2024-01-09 13:35:55 +0700

    อัพเดท Product

--- database/migrations/2024_01_09_130037_create_products_table.php
+++ database/migrations/2024_01_09_130037_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name',255)->comment("ชื่อสินค้า");
             $table->string('price',255)->comment("ราคาสินค้า");
             $table->text('description')->comment("รายระเอียดประเภทสินค้า");
-            $table->integer('category_id')->comment("รหัสประเภทสินค้า");
+            $table->integer('category_id')->comment("รหัสปรเภทสินค้า");
             $table->string('image',255)->comment("รูปภาพ");
             $table->timestamps();
         });
